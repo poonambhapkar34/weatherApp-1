ada0ee96b8c011c03fb57afd8e3fe35ff438957b
pbhapkar17 <poonambhapkar17@gmail.com>
2023-08-20 18:00:42 +0530

    weather app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainContentComponent } from './main-content/main-content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'main',component:MainContentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
